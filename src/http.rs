1ac176451f8c7ac5a25b4279358b0e342b7b0774
Steve Cooper <steve@stevecooper.org>
2024-04-14 09:53:29 +0100

    Commit Sun Apr 14 09:53:29 BST 2024

--- src/http.rs
+++ src/http.rs
@@ -1,38 +1,26 @@
 use anyhow::Result;
-use http::Uri;
-use hyper::body::Buf;
-use hyper::Client;
-use hyper_tls::HttpsConnector;
+use http::{Uri};
 use serde::de::DeserializeOwned;
 
 pub async fn fetch_json<T>(url: &Uri) -> Result<T>
 where
     T: DeserializeOwned,
 {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
-    // Fetch the url...
-    let res = client.get(url.clone()).await?;
-
-    // asynchronously aggregate the chunks of the body
-    let body = hyper::body::aggregate(res).await?;
-
-    // try to parse as json with serde_json
-    let reader = body.reader();
-    let result = serde_json::from_reader(reader)?;
+    let reqwest_url = url.to_string();
+    let result = reqwest::get(reqwest_url)
+        .await?
+        .json::<T>()
+        .await?;
 
     Ok(result)
 }
 
 pub async fn fetch_bytes(url: &Uri) -> Result<Vec<u8>> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
-    // Fetch the url...
-    let res = client.get(url.clone()).await?;
-
-    // return the body as bytes
-    let bytes = hyper::body::to_bytes(res.into_body()).await?;
+    let reqwest_url = url.to_string();
+    let bytes = reqwest::get(reqwest_url)
+        .await?
+        .bytes()
+        .await?
+        .to_vec();
     Ok(bytes.to_vec())
 }
